f400471eac8851db9473312bd78233f6bca0bbb7
hiroyuki9614 <mmnnoov@gmail.com>
2024-07-24 23:59:11 +0900

    APIの実装

--- prisma/migrations/20240724133252_increase_description_length/migration.sql
+++ prisma/migrations/20240724133252_increase_description_length/migration.sql
@@ -1,18 +1,9 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Todo` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Todo`;
-
 -- CreateTable
-CREATE TABLE `studies_memo` (
+CREATE TABLE `study_memo` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `study_date` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `title` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL DEFAULT '',
+    `description` TEXT NOT NULL,
     `duration` INTEGER NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updated_at` DATETIME(3) NOT NULL,
