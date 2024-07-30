30369e00cffcb8d17ce2a00ca0433c09519eb9af
hiroyuki9614 <mmnnoov@gmail.com>
2024-07-30 23:59:39 +0900

    ボタンコンポーネント追加

--- src/app/udemy_test/page.tsx
+++ src/app/udemy_test/page.tsx
@@ -1,15 +1,21 @@
+'use client'
 import Hello from "../component/ui/udemy-component/hello";
 import Bye from "../component/ui/udemy-component/bye";
 import Expression from "../component/ui/udemy-component/expression";
 import List from "../component/ui/udemy-component/list";
+import Button from "../component/ui/button/button";
+
 
 const Example = () => {
 	const name="test"
+	const handleClick = () => {
+		alert('ボタンがクリックされました！');
+	  };
 	return (
 		<>
-			<Hello name={name} />
-			<Bye name={name} />
-			<Expression />
+			<Button onClick={handleClick} variant="green-fill">クリックしてください</Button>
+			<Button onClick={handleClick} disabled variant="red-gradation">無効なボタン</Button>
+			<Button onClick={handleClick} type="submit" className="primary-button" variant="variantのkey名3">送信</Button>
 		</>
 	)
 }
