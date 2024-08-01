6c398890cb0d44e2a46defb9f48937dcb36e4a2f
hiroyuki9614 <mmnnoov@gmail.com>
2024-08-01 21:45:12 +0900

    Udemy props

--- src/app/udemy_test/page.tsx
+++ src/app/udemy_test/page.tsx
@@ -4,19 +4,19 @@ import Bye from "../component/ui/udemy-component/bye";
 import Expression from "../component/ui/udemy-component/expression";
 import List from "../component/ui/udemy-component/list";
 import Button from "../component/ui/button/button";
-
+import { useState } from "react";
 
 const Example = () => {
-	const name="test"
-	const handleClick = () => {
-		alert('ボタンがクリックされました！');
-	  };
+	const personObj: { name: string, age: number } = { name: "John", age: 12 }
+	const [person, setPerson] = useState(personObj);
+	console.log(person);
+	const changePerson = (e: any) => {
+		setPerson({ name: e.target.value, age: person.age});
+	}
 	return (
 		<>
-			<button>Click Me!</button>
-			<Button onClick={handleClick} variant="green-fill">クリックしてください</Button>
-			<Button onClick={handleClick} disabled variant="red-gradation">無効なボタン</Button>
-			<Button onClick={handleClick} type="submit" className="primary-button" variant="variantのkey名3">送信</Button>
+			<p>{person.name}</p><p>{person.age.toString()}</p>
+			<input type="text" onChange={changePerson} value={`${person.name}`} />
 		</>
 	)
 }
