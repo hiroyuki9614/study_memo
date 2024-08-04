3e330eb06db09efea1c6d6635d6096b7fee52df0
hiroyuki9614 <mmnnoov@gmail.com>
2024-08-04 19:59:51 +0900

    Udemy props

--- src/app/test/page.jsx
+++ src/app/test/page.jsx
@@ -1,9 +1,24 @@
-"use client"
+class Book {
+	constructor(author, title, isbn) {
+		this.title = title;
+		this.author = author;
+		this.isbn = isbn;
+		this.isCheckedOut = false;
+	}
+	checkOut() {
+		if (!this.isCheckedOut) {
+		this.isCheckedOut = true;
+		return true;
+		}
+		return false;
+	}
+	returnBook() {
 
-const jsTest = () => {
-	return (
-		<></>
-	);
-};
+	}
+}
 
-export default jsTest;
+
+// 要件：
+
+// Bookクラスには、タイトル、著者、ISBN、貸出状況などのプロパティを持たせます。
+// Libraryクラスには、本のコレクションと、本の追加、貸出、返却などのメソッドを実装します。
