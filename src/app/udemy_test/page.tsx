00473fa548d7fef53a980231f0ead437d3d0b5d1
hiroyuki9614 <mmnnoov@gmail.com>
2024-08-10 20:14:07 +0900

    python

--- src/app/udemy_test/page.tsx
+++ src/app/udemy_test/page.tsx
@@ -1,36 +1,39 @@
 "use client"
-import { useState } from "react";
-import { createPortal } from "react-dom";
-import Modal from "../component/ui/udemy-component/Modal";
-
-/* POINT createPortalの使い方
-第一引数: React の子要素としてレンダー可能なもの （要素、文字列、フラグメント、コンポーネントなど）
-第二引数: レンダー先のDOM要素
-*/
-
-/* POINT createPortalはどんなときに使うか？
-子要素は親要素のスタイルによって表示に制限を受ける場合があります。
-（overflow: hidden 、 z-index 、 width　など・・・ ）
-それらの制限なく、子要素が親要素を「飛び出して」表示する必要があるときにcreatePortalを使うのが有効です。
-モーダル、ポップアップ、トーストは使用の代表例です。
-*/
+import { useReducer, useState } from "react"
 
 const Example = () => {
-  const [modalOpen, setModalOpen] = useState(false);
-  return (
-    <div>
-      <div className="container start">
+  const [state, setState] = useState(0);
 
-      <button
-        type="button"
-        onClick={() => setModalOpen(true)}
-        disabled={modalOpen}
-      >
-        モーダルを表示する
-      </button>
-      {modalOpen && <Modal handleCloseClick={() => setModalOpen(false)} />}
-	  </div>
-    </div>
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if(action === '+') {
+      return ++prev;
+    } else if (action === '-') {
+      return --prev;
+    }
+  }, 0);
+  const countUp = () => {
+    setState(prev => {
+      return prev = ++prev;
+    })
+  }
+  const rcountUp = () => {
+    dispatch('+');
+  }
+  const rcountDown = () => {
+    dispatch('-');
+  }
+  return (
+    <>
+      <div>
+        <h3>{state}</h3>
+        <button onClick={countUp}>+</button>
+      </div>
+      <div>
+        <h3>{rstate}</h3>
+        <button onClick={rcountUp}>+</button>
+        <button onClick={rcountDown}>-</button>
+      </div>
+    </>
   );
 };
 
