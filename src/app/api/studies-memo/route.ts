28199d700b068d069642845dbb069f22512cdf5f
hiroyuki9614 <mmnnoov@gmail.com>
2024-08-14 23:20:17 +0900

    python

--- src/app/api/studies-memo/route.ts
+++ src/app/api/studies-memo/route.ts
@@ -1,3 +1,4 @@
+"use server"
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { useState } from 'react';
 import React, { FormEvent } from 'react';
+import {createPost} from '../api/studies-memo/route'
 
 const CreateStudiesMemo = () => {
 	const [title, setTitle] = useState('');
 	const [duration, setDuration] = useState('');
 	const [description, setDescription] = useState('');
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+	const handleSubmit = async (e: any) => {
 		e.preventDefault();
 		try {
 			const response = await fetch('http://localhost:3000/api/studies-memo', {
@@ -31,7 +32,7 @@ const CreateStudiesMemo = () => {
 	return (
 		<div>
 			<h1>学習メモ作成</h1>
-			<form onSubmit={handleSubmit}>
+			<form action={createPost}>
 				<input type='text' onChange={(e) => setTitle(e.target.value)} name='title' placeholder='タイトル' required />
 				<input type='number' onChange={(e) => setDuration(e.target.value)} name='duration' placeholder='学習時間' required />
 				<textarea name='description' onChange={(e) => setDescription(e.target.value)} rows={15} placeholder='学習内容' required></textarea>
