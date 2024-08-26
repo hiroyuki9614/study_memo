db304ab667265753b32154464b08bf2aca138d40
hiroyuki9614 <mmnnoov@gmail.com>
2024-08-26 23:07:59 +0900

    月修正コンポーネントの実装

--- src/app/component/ui/studiesList/index.tsx
+++ src/app/component/ui/studiesList/index.tsx
@@ -24,7 +24,8 @@ interface MonthlyStudiesProps {
 }
 
 const ReadAllStudiesData: React.FC<MonthlyStudiesProps> = ({ allStudiesData, year }) => {
-	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const [isOpenYear, setIsOpenYear] = useState<boolean>(false);
+	const [openMonths, setOpenMonths] = useState<Record<string, boolean>>({});
 	// 学習時間を月ごとに分ける
 	const studiesByMonth = allStudiesData.reduce((acc, study) => {
 		const month = dayjs(study.created_at).format('MM');
@@ -44,44 +45,57 @@ const ReadAllStudiesData: React.FC<MonthlyStudiesProps> = ({ allStudiesData, yea
 
 	// 月の順序を1月から12月に並べ替え 出力 0102(省略)101112
 	const sortedMonths = Object.keys(monthlyStudies).sort((a, b) => parseInt(a) - parseInt(b));
-	// console.log(monthlyStudies);
+	const handleYearClick = () => {
+		setIsOpenYear(!isOpenYear);
+	};
+
+	const handleMonthClick = (month: string) => {
+		setOpenMonths((prev) => ({
+			...prev,
+			[month]: !prev[month],
+		}));
+	};
 	return (
-		<article className='flex items-center box-border flex-col justify-center min-h-screen p-5'>
-			<div className='bg-slate-50 rounded-2xl shadow shadow-slate-500 mb-4 p-4 w-10/12'>
+		<article className='flex items-center box-border flex-col justify-center pt-2'>
+			<div className='bg-slate-50 rounded-2xl shadow shadow-slate-500 mb-4 p-4 w-10/12' onClick={handleYearClick}>
 				<h2 className='study-record__month'>{year}年の学習内容</h2>
 			</div>
-			<p>{}</p>
-			{sortedMonths.map((month) => (
-				<div key={month} className='bg-slate-50 rounded-2xl shadow shadow-slate-500 mb-4 p-4 w-10/12'>
-					<h2 className='text-lg text-center pb-3'>{parseInt(month)}月の学習内容</h2>
-					<div className=''>
-						{monthlyStudies[month].length > 0 ? (
-							<ul>
-								{monthlyStudies[month].map((studyData) => (
-									<li key={studyData._id} className='[&:not(:last-child)]:border-b border-1 px-2 p-4'>
-										<dl>
-											<div>
-												<dt>記録日時:</dt>
-												<dd className='indent-4'>{dayjs(studyData.created_at).format('YYYY年MM月DD日 HH:mm')}</dd>
-											</div>
-											<div>
-												<dt>学習時間</dt>
-												<dd className='indent-4'>{studyData.duration}h</dd>
-											</div>
-											<div className=''>
-												<dt>学習内容</dt>
-												<dd className='indent-4'>学習内容: {studyData.title}</dd>
-											</div>
-										</dl>
-									</li>
-								))}
-							</ul>
-						) : (
-							<p>この月の学習記録はありません。</p>
+			{isOpenYear &&
+				sortedMonths.map((month) => (
+					<div key={month} className='bg-slate-50 rounded-2xl shadow shadow-slate-500 mb-4 p-4 w-10/12'>
+						<h2 className='text-lg text-center pb-3' onClick={() => handleMonthClick(month)}>
+							{parseInt(month)}月の学習内容
+						</h2>
+						{openMonths[month] && (
+							<div className=''>
+								{monthlyStudies[month].length > 0 ? (
+									<ul>
+										{monthlyStudies[month].map((studyData) => (
+											<li key={studyData._id} className='[&:not(:last-child)]:border-b border-1 px-2 p-4'>
+												<dl>
+													<div>
+														<dt>記録日時:</dt>
+														<dd className='indent-4'>{dayjs(studyData.created_at).format('YYYY年MM月DD日 HH:mm')}</dd>
+													</div>
+													<div>
+														<dt>学習時間</dt>
+														<dd className='indent-4'>{studyData.duration}h</dd>
+													</div>
+													<div className=''>
+														<dt>学習内容</dt>
+														<dd className='indent-4'>学習内容: {studyData.title}</dd>
+													</div>
+												</dl>
+											</li>
+										))}
+									</ul>
+								) : (
+									<p>この月の学習記録はありません。</p>
+								)}
+							</div>
 						)}
 					</div>
-				</div>
-			))}
+				))}
 		</article>
 	);
 };
