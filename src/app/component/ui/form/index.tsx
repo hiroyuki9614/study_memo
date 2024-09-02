742e1f6341d1ba76b8e58617f582d3438e15a26f
hiroyuki9614 <mmnnoov@gmail.com>
2024-09-02 22:22:17 +0900

    inputの修正

--- src/app/component/ui/form/index.tsx
+++ src/app/component/ui/form/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { useRef } from 'react';
 import InputText from '../inputText/index';
 import InputTextArea from '../text-area';
 import { POST } from '../../../api/studies-memo/route';
@@ -10,24 +11,33 @@ type Inputs = {
 	title: string;
 	duration: number;
 	description: string;
-	// 他のフィールドもここに追加
 };
 
 export default function CreateStudiesMemo() {
+	const formRef = useRef<HTMLFormElement>(null);
 	const {
 		register,
 		handleSubmit,
 		reset,
 		formState: { errors },
 	} = useForm<Inputs>();
-	const onSubmit: SubmitHandler<Inputs> = () => reset();
+
+	const onSubmit: SubmitHandler<Inputs> = async (data) => {
+		if (formRef.current) {
+			const formData = new FormData(formRef.current);
+			const result = await POST(formData);
+			if (result.message === 'success') {
+				reset();
+			}
+		}
+	};
 
 	return (
-		<form action={POST}>
-			<InputText id='studyTitle' title='タイトル' name={'title'} register={register} errors={errors} type='text' required />
+		<form ref={formRef} action={POST} onSubmit={handleSubmit(onSubmit)}>
+			<InputText id='studyTitle' title='タイトル' name='title' register={register} errors={errors} type='text' required />
 			<InputText id='studyDuration' title='学習時間' name='duration' register={register} errors={errors} type='number' required />
 			<InputTextArea id='studyDescription' title='学習内容' name='description' register={register} errors={errors} required />
-			<Button icon={faUser} type='submit' variant='primary' onClick={() => reset()}>
+			<Button icon={faUser} type='submit' variant='primary'>
 				送信
 			</Button>
 		</form>
