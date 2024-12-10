57b55304ab0ff4410383bfdaf878a5ca5afc56d0
hiroyuki9614 <mmnnoov@gmail.com>
2024-12-10 23:53:16 +0900

    jest

---
+++ __test__/components/ButtonComponents.test.jsx
@@ -0,0 +1,19 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import ButtonComponents from '../../src/app/components/ButtonComponent'
+
+describe('ButtonComponent', () => {
+  it('ボタンをクリックするとテキストが変更される', () => {
+    render(<ButtonComponents />);
+
+    // 初期テキストの確認
+    expect(screen.getByText('初期テキスト')).toBeInTheDocument();
+
+    // ボタンのクリック
+    fireEvent.click(screen.getByText('ボタンをクリック'));
+
+    // 更新されたテキストの確認
+    expect(screen.getByText('更新されたテキスト')).toBeInTheDocument();
+  });
+});
