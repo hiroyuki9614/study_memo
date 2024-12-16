a4211e027a66ab02d02282681370a5f72bd31761
hiroyuki9614 <mmnnoov@gmail.com>
2024-12-16 23:42:00 +0900

    jest

--- __tests__/api/users.test.ts
+++ __tests__/api/users.test.ts
@@ -1,18 +1,22 @@
-import { createMocks } from 'node-mocks-http';
-import handler from './users.page';
+import { testApiHandler } from 'next-test-api-route-handler'; // ◄ Must be first import
+// Import the handler under test from the app directory
+import * as appHandler from '../../src/app/api/api';
 
-describe('/api/users', () => {
-	test('returns a list of users', async () => {
-		const { req, res } = createMocks({
-			method: 'GET',
-		});
-
-		const response = await handler(req);
-
-		expect(response.status).toBe(200);
-		expect(await response.json()).toEqual([
-			{ id: 1, name: 'Alice' },
-			{ id: 2, name: 'Bob' },
-		]);
+it('does what I want', async () => {
+	await testApiHandler({
+		appHandler,
+		// requestPatcher is optional
+		requestPatcher(request) {
+			request.headers.set('key', process.env.SPECIAL_TOKEN);
+		},
+		// responsePatcher is optional
+		async responsePatcher(response) {
+			const json = await response.json();
+			return Response.json(json.apiSuccess ? { hello: 'world!' } : { goodbye: 'cruel world' });
+		},
+		async test({ fetch }) {
+			const res = await fetch({ method: 'POST', body: 'dummy-data' });
+			await expect(res.json()).resolves.toStrictEqual({ hello: 'world!' }); // ◄ Passes!
+		},
 	});
 });
